b462d11178be950cfad5c766fa5c37596b5b44b9
zhangsr <zhangsr@tiaohuo.com>
2018-02-22 17:34:15 +0800

    新接口

--- src/net/wit/plugin/weixin/WeiXinPayPlugin.java
+++ src/net/wit/plugin/weixin/WeiXinPayPlugin.java
@@ -6,11 +6,15 @@
 package net.wit.plugin.weixin;
 
 import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.math.BigDecimal;
+import java.security.KeyStore;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.net.ssl.SSLContext;
 import javax.servlet.http.HttpServletRequest;
 
 import net.wit.entity.BindUser;
@@ -21,11 +25,17 @@ import net.wit.plugin.PaymentPlugin;
 import net.wit.util.MD5Utils;
 import net.wit.plat.weixin.util.WeiXinUtils;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
 import org.apache.http.util.EntityUtils;
 import org.springframework.stereotype.Component;
 
@@ -282,6 +292,55 @@ public class WeiXinPayPlugin extends PaymentPlugin {
 	@Override
 	public Integer getTimeout() {
 		return 30;
+	}
+
+	/**
+	 * https双向签名认证，用于支付申请退款
+	 *
+	 * */
+	public String payHttps(String url,String xml,HttpServletRequest request) throws Exception {
+		PluginConfig pluginConfig = getPluginConfig();
+		//指定读取证书格式为PKCS12
+		KeyStore keyStore = KeyStore.getInstance("PKCS12");
+		String rootPath = request.getSession().getServletContext().getRealPath("/");
+		String path = rootPath+"/cert/weixin.pfx";
+		//读取本机存放的PKCS12证书文件
+		FileInputStream instream = new FileInputStream(new File(path));
+		try {
+			//指定PKCS12的密码(商户ID)
+			keyStore.load(instream,pluginConfig.getAttribute("partner").toCharArray());
+		} finally {
+			instream.close();
+		}
+		SSLContext sslcontext = SSLContexts.custom().loadKeyMaterial(keyStore,pluginConfig.getAttribute("partner").toCharArray()).build();
+		//指定TLS版本
+		SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+				sslcontext,new String[] { "TLSv1" },null,
+				SSLConnectionSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);
+		//设置httpclient的SSLSocketFactory
+		CloseableHttpClient httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+		try {
+			HttpPost httpost = new HttpPost(url); // 设置响应头信息
+			httpost.addHeader("Connection", "keep-alive");
+			httpost.addHeader("Accept", "*/*");
+			httpost.addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+			httpost.addHeader("Host", "api.mch.weixin.qq.com");
+			httpost.addHeader("X-Requested-With", "XMLHttpRequest");
+			httpost.addHeader("Cache-Control", "max-age=0");
+			httpost.addHeader("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0) ");
+			httpost.setEntity(new StringEntity(xml, "UTF-8"));
+			CloseableHttpResponse response = httpclient.execute(httpost);
+			try {
+				HttpEntity entity = response.getEntity();
+				String jsonStr = EntityUtils.toString(response.getEntity(), "UTF-8");
+				EntityUtils.consume(entity);
+				return jsonStr;
+			}finally {
+				response.close();
+			}
+		}finally {
+			httpclient.close();
+		}
 	}
 
 }
