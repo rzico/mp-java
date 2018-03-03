4da57737368a358b4b66656394b713168304a11d
zhangsr <zhangsr@tiaohuo.com>
2018-03-03 19:45:30 +0800

    新接口

--- src/net/wit/controller/applet/ArticleController.java
+++ src/net/wit/controller/applet/ArticleController.java
@@ -2,9 +2,9 @@ package net.wit.controller.applet;
 
 import net.wit.*;
 import net.wit.Message;
-import net.wit.Order;
 import net.wit.controller.admin.BaseController;
 import net.wit.controller.model.ArticleListModel;
+import net.wit.controller.model.ArticlePreviewModel;
 import net.wit.controller.model.ArticleViewModel;
 import net.wit.controller.model.GoodsListModel;
 import net.wit.entity.*;
@@ -17,7 +17,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -25,7 +27,7 @@ import java.util.List;
  * @author 降魔战队
  * @date 2017-9-14 19:42:9
  */
- 
+
 @Controller("appletArticleController")
 @RequestMapping("applet/article")
 public class ArticleController extends BaseController {
@@ -48,21 +50,27 @@ public class ArticleController extends BaseController {
     @Resource(name = "goodsServiceImpl")
     private GoodsService goodsService;
 
-    @Resource(name = "articleCatalogServiceImpl")
-    private ArticleCatalogService articleCatalogService;
+    @Resource(name = "articleLaudServiceImpl")
+    private ArticleLaudService articleLaudService;
+
+    @Resource(name = "articleFavoriteServiceImpl")
+    private ArticleFavoriteService articleFavoriteService;
 
     @Resource(name = "articleCategoryServiceImpl")
     private ArticleCategoryService articleCategoryService;
+
+    @Resource(name = "articleCatalogServiceImpl")
+    private ArticleCatalogService articleCatalogService;
 
     @Resource(name = "memberFollowServiceImpl")
     private MemberFollowService memberFollowService;
 
     /**
-     * 文章预览信息
+     * 文章预览详情
      */
     @RequestMapping(value = "/view", method = RequestMethod.GET)
     @ResponseBody
-    public Message view(Long id,Long xuid,HttpServletRequest request){
+    public Message view(Long id,HttpServletRequest request){
         Article article = articleService.find(id);
         if (article==null) {
             return Message.error("无效文章编号");
@@ -79,21 +87,53 @@ public class ArticleController extends BaseController {
         }
         ArticleViewModel model =new ArticleViewModel();
         model.bind(article,member);
-        Member share = null;
-        if (xuid!=null) {
-            share = memberService.find(xuid);
-        }
-        if (share!=null) {
-            model.setShareNickName(share.getNickName());
-        } else {
-            model.setShareNickName(article.getMember().getNickName());
-        }
+        Map<String,Object> data = new HashMap<String,Object>();
+        data.put("article",model);
 
-        return Message.bind(model,request);
+        ArticlePreviewModel option =new ArticlePreviewModel();
+        option.bind(article);
+        if (member!=null) {
+
+            List<Filter> filters = new ArrayList<Filter>();
+            filters.add(new Filter("member", Filter.Operator.eq,member));
+            filters.add(new Filter("article", Filter.Operator.eq,article));
+            List<ArticleFavorite> favorites = articleFavoriteService.findList(null,null,filters,null);
+            option.setHasFavorite(favorites.size()>0);
+
+            List<Filter> laudfilters = new ArrayList<Filter>();
+            laudfilters.add(new Filter("member", Filter.Operator.eq,member));
+            laudfilters.add(new Filter("article", Filter.Operator.eq,article));
+            List<ArticleLaud> lauds = articleLaudService.findList(null,null,laudfilters,null);
+            option.setHasLaud(lauds.size()>0);
+
+            MemberFollow memberFollow = memberFollowService.find(member, article.getMember());
+            option.setHasFollow(memberFollow!=null);
+
+        }
+        data.put("option",option);
+
+        return Message.bind(data,request);
    }
 
+     /**
+     * 获取显示模版
+     */
+    @RequestMapping(value = "/template", method = RequestMethod.POST)
+    @ResponseBody
+    public Message template(Long id,HttpServletRequest request){
+        Article article = articleService.find(id);
+        if (article==null) {
+            return Message.error("无效文章编号");
+        }
+        String sn="1001";
+        if (article.getTemplate()==null) {
+            sn = article.getTemplate().getSn();
+        }
+        return Message.success((Object)sn,"发布成功");
+    }
+
     /**
-     *  文章列表,带分页
+     *  分类查询列表
      *  会员 id
      */
     @RequestMapping(value = "/list", method = RequestMethod.GET)
@@ -117,17 +157,36 @@ public class ArticleController extends BaseController {
         } else {
             filters.add(new Filter("isAudit", Filter.Operator.eq, true));
         }
-        filters.add(new Filter("isPublish", Filter.Operator.eq, true));
         filters.add(new Filter("authority", Filter.Operator.eq, Article.Authority.isPublic));
+        filters.add(new Filter("isPublish", Filter.Operator.eq, true));
 
         pageable.setFilters(filters);
-        pageable.setOrderProperty("hits");
-        pageable.setOrderDirection(Order.Direction.desc);
         Page<Article> page = articleService.findPage(null,null,null,pageable);
         PageBlock model = PageBlock.bind(page);
         model.setData(ArticleListModel.bindList(page.getContent()));
         return Message.bind(model,request);
     }
+
+
+    /**
+     *  文章搜索
+     *  keyword
+     */
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    @ResponseBody
+    public Message search(String keyword,Pageable pageable, HttpServletRequest request){
+        if (keyword==null) {
+            return Message.error("请输入关键词");
+        }
+        List<Filter> filters = new ArrayList<Filter>();
+        filters.add(Filter.like("title","%"+keyword+"%"));
+        pageable.setFilters(filters);
+        Page<Article> page = articleService.findPage(null,null,null,pageable);
+        PageBlock model = PageBlock.bind(page);
+        model.setData(ArticleListModel.bindList(page.getContent()));
+        return Message.bind(model,request);
+    }
+
 
     /**
      *  商品信息
@@ -143,5 +202,6 @@ public class ArticleController extends BaseController {
         model.bind(goods);
         return Message.bind(model,request);
     }
+
 
 }
