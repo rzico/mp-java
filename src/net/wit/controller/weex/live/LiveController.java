d96c0bf49e61749bfdacaef1611ceb7e71c34846
zhangsr <zhangsr@tiaohuo.com>
2018-04-07 00:17:50 +0800

    新接口

--- src/net/wit/controller/weex/live/LiveController.java
+++ src/net/wit/controller/weex/live/LiveController.java
@@ -6,6 +6,7 @@ import net.wit.Page;
 import net.wit.Pageable;
 import net.wit.controller.admin.BaseController;
 import net.wit.controller.model.LiveModel;
+import net.wit.controller.model.LiveTapeModel;
 import net.wit.controller.model.MemberModel;
 import net.wit.entity.Live;
 import net.wit.entity.LiveTape;
@@ -83,7 +84,83 @@ public class LiveController extends BaseController {
      */
     @RequestMapping(value = "/play", method = RequestMethod.POST)
     @ResponseBody
-    public Message  play(Long id,HttpServletRequest request){
+    public Message  play(Long id,String location,HttpServletRequest request){
+        Member member = memberService.getCurrent();
+        if (member==null) {
+            return Message.error(Message.SESSION_INVAILD);
+        }
+        Live live = liveService.find(id);
+        if (live == null) {
+            return Message.error("无效直播id");
+        }
+
+
+        String playUrl = "";
+        String pushUrl = "";
+        LiveTape liveTape = new LiveTape();
+        liveTape.setLive(live);
+        liveTape.setMember(member);
+        liveTape.setFrontcover(live.getFrontcover());
+        liveTape.setHeadpic(member.getLogo());
+        liveTape.setNickname(member.displayName());
+        liveTape.setGift(0L);
+        liveTape.setLikeCount(0L);
+        liveTape.setViewerCount(0L);
+        liveTape.setLocation(live.getLocation());
+        liveTape.setPlayUrl(playUrl);
+        liveTape.setPushUrl(pushUrl);
+        liveTape.setTitle(live.getTitle());
+        liveTape.setLocation(location);
+        liveTapeService.save(liveTape);
+
+        live.setLiveTape(liveTape);
+        live.setPushUrl(pushUrl);
+        live.setPlayUrl(playUrl);
+        live.setOnline("1");
+        liveService.update(live);
+
+        LiveTapeModel model = new LiveTapeModel();
+        model.bind(liveTape);
+        return Message.success(model,"success");
+    }
+
+
+
+    /**
+     *   结束直播
+     */
+    @RequestMapping(value = "/stop", method = RequestMethod.POST)
+    @ResponseBody
+    public Message  stop(Long id,String location,HttpServletRequest request){
+        Member member = memberService.getCurrent();
+        if (member==null) {
+            return Message.error(Message.SESSION_INVAILD);
+        }
+        Live live = liveService.find(id);
+        if (live == null) {
+            return Message.error("无效直播id");
+        }
+        LiveTape liveTape = live.getLiveTape();
+
+        liveTape.setTitle(live.getTitle());
+        liveTape.setLocation(location);
+        liveTapeService.save(liveTape);
+
+        live.setOnline("0");
+        liveService.update(live);
+
+        LiveTapeModel model = new LiveTapeModel();
+        model.bind(liveTape);
+        return Message.success(model,"success");
+    }
+
+
+    /**
+     *   进入房间
+     */
+    @RequestMapping(value = "/into", method = RequestMethod.POST)
+    @ResponseBody
+    public Message  into(Long id,String location,HttpServletRequest request){
         Member member = memberService.getCurrent();
         if (member==null) {
             return Message.error(Message.SESSION_INVAILD);
@@ -108,6 +185,7 @@ public class LiveController extends BaseController {
         liveTape.setPlayUrl(playUrl);
         liveTape.setPushUrl(pushUrl);
         liveTape.setTitle(live.getTitle());
+        liveTape.setLocation(location);
         liveTapeService.save(liveTape);
 
         live.setLiveTape(liveTape);
@@ -116,10 +194,41 @@ public class LiveController extends BaseController {
         live.setOnline("1");
         liveService.update(live);
 
-        LiveModel model = new LiveModel();
-        model.bind(live);
+        LiveTapeModel model = new LiveTapeModel();
+        model.bind(liveTape);
         return Message.success(model,"success");
     }
+
+
+
+    /**
+     *   退出房间
+     */
+    @RequestMapping(value = "/quit", method = RequestMethod.POST)
+    @ResponseBody
+    public Message  quit(Long id,String location,HttpServletRequest request){
+        Member member = memberService.getCurrent();
+        if (member==null) {
+            return Message.error(Message.SESSION_INVAILD);
+        }
+        Live live = liveService.find(id);
+        if (live == null) {
+            return Message.error("无效直播id");
+        }
+        LiveTape liveTape = live.getLiveTape();
+
+        liveTape.setTitle(live.getTitle());
+        liveTape.setLocation(location);
+        liveTapeService.save(liveTape);
+
+        live.setOnline("0");
+        liveService.update(live);
+
+        LiveTapeModel model = new LiveTapeModel();
+        model.bind(liveTape);
+        return Message.success(model,"success");
+    }
+
 
 
 }
