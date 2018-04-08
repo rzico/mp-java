038dc14d0fe4cdb8fe63c426516bea6c4d001d66
zhangsr <zhangsr@tiaohuo.com>
2018-04-08 23:37:48 +0800

    im

--- src/net/wit/entity/ArticleReward.java
+++ src/net/wit/entity/ArticleReward.java
@@ -83,11 +83,11 @@ public class ArticleReward extends BaseEntity {
 	private BigDecimal fee;
 
 	/** 交易状态 */
-	@Column(columnDefinition="int(11) not null comment '交易状态 {wait:等待支付,success:支付成功,failure:支付失败}'")
+	@Column(columnDefinition="int(11) not null default 0 comment '交易状态 {wait:等待支付,success:支付成功,failure:支付失败}'")
 	private Status status;
 
 	/** 打赏类型 */
-	@Column(columnDefinition="int(11) not null comment '打赏类型'")
+	@Column(columnDefinition="int(11) not null default 0 comment '打赏类型'")
 	private Type type;
 
 	public String getIp() {
