5304680be95c010dcad15e12554e8f04ae3fa35e
Eric <q1005310916@gmail.com>
2018-04-10 12:06:44 +0800

    1.圈子去掉了member的过滤

--- src/net/wit/controller/weex/ArticleController.java
+++ src/net/wit/controller/weex/ArticleController.java
@@ -217,7 +217,6 @@ public class ArticleController extends BaseController {
         List<Filter> filters = new ArrayList<Filter>();
         if (id!=null) {
             member = memberService.find(id);
-            filters.add(new Filter("member", Filter.Operator.eq,member));
         } else {
             filters.add(new Filter("isAudit", Filter.Operator.eq,true));
             tags = tagService.findList(4L,5L);
