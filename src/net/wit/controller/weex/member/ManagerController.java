f5aee4d520004d34ef5302c37eaf01b8737aad7c
zhangsr <zhangsr@tiaohuo.com>
2018-04-13 23:32:19 +0800

    im

--- src/net/wit/controller/weex/member/ManagerController.java
+++ src/net/wit/controller/weex/member/ManagerController.java
@@ -1,9 +1,11 @@
 package net.wit.controller.weex.member;
 
+import net.wit.Filter;
 import net.wit.Message;
 import net.wit.controller.admin.BaseController;
 import net.wit.controller.model.ManagerModel;
 import net.wit.entity.Admin;
+import net.wit.entity.Enterprise;
 import net.wit.entity.Member;
 import net.wit.service.*;
 import org.springframework.stereotype.Controller;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -49,6 +53,9 @@ public class ManagerController extends BaseController {
     @Resource(name = "adminServiceImpl")
     private AdminService adminService;
 
+    @Resource(name = "shopServiceImpl")
+    private ShopService shopService;
+
     /**
      * 获取管理界面首页状态
      */
@@ -62,6 +69,19 @@ public class ManagerController extends BaseController {
 
         ManagerModel model =new ManagerModel();
         model.bind(member);
+
+        Admin admin = adminService.findByMember(member);
+        if (admin!=null && admin.getEnterprise()!=null) {
+            Enterprise ent = admin.getEnterprise();
+            if (!ent.getType().equals(Enterprise.Type.shop) && ent.getStatus().equals(Enterprise.Status.success)) {
+                model.setIsAgent(true);
+            }
+            if (ent.getType().equals(Enterprise.Type.shop)) {
+                model.setIsShop(true);
+            }
+        }
+        Long shops = shopService.count(new Filter("enterprise", Filter.Operator.eq,admin.getEnterprise()));
+        model.setHasShop(shops>0L);
 
         return Message.bind(model,request);
    }
