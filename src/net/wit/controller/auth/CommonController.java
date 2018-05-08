beaf7b07747fc5d89b6b13c198848847ee1379d8
chenjinle <jinlesoft@163.com>
2018-05-08 10:27:56 +0800

    remember

---
+++ src/net/wit/controller/auth/CommonController.java
@@ -0,0 +1,15 @@
+package net.wit.controller.auth;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Jinlesoft on 2018/5/8.
+ */
+@Controller("commonController")
+@RequestMapping("/common")
+public class CommonController extends BaseController{
+
+
+
+}
