0ccbc770e5b203bdc1cb9d7b2e923ed72ca466f0
chenjinle <jinlesoft@163.com>
2018-05-11 18:03:20 +0800

    remember

---
+++ src/net/wit/entity/weixin/WeiXinCallBack.java
@@ -0,0 +1,25 @@
+package net.wit.entity.weixin;
+
+/**
+ * Created by Jinlesoft on 2018/5/9.
+ */
+public class WeiXinCallBack {
+    private String errcode;
+    private String errmsg;
+
+    public String getErrcode() {
+        return errcode;
+    }
+
+    public void setErrcode(String errcode) {
+        this.errcode = errcode;
+    }
+
+    public String getErrmsg() {
+        return errmsg;
+    }
+
+    public void setErrmsg(String errmsg) {
+        this.errmsg = errmsg;
+    }
+}
