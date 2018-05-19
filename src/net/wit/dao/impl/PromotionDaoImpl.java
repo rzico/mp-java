b84497834b679777e71b3b9683a2f117915c4686
zhangsr <zhangsr@tiaohuo.com>
2018-05-19 20:38:30 +0800

    im

--- src/net/wit/dao/impl/PromotionDaoImpl.java
+++ src/net/wit/dao/impl/PromotionDaoImpl.java
@@ -54,6 +54,9 @@ public class PromotionDaoImpl extends BaseDaoImpl<Promotion, Long> implements Pr
 			e =DateUtils.addDays(e,1);
 			restrictions = criteriaBuilder.and(restrictions,criteriaBuilder.lessThan(root.<Date> get("createDate"), e));
 		}
+
+		restrictions = criteriaBuilder.and(restrictions,criteriaBuilder.equal(root.<Boolean> get("deleted"), false));
+
 		criteriaQuery.where(restrictions);
 		return super.findPage(criteriaQuery,pageable);
 	}
