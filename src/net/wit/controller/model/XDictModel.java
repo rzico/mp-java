4025384e569545ce12cbd146e2bfb453be8fb554
zhangsr <zhangsr@tiaohuo.com>
2018-05-31 21:27:42 +0800

    im

--- src/net/wit/controller/model/XDictModel.java
+++ src/net/wit/controller/model/XDictModel.java
@@ -11,6 +11,14 @@ public class XDictModel extends BaseModel implements Serializable {
 
     private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void bind(Xdict xdict) {
         this.name = xdict.getName();
     }
