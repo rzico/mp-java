298d2183e3876b1431bc4dca954fd6dd652cad63
zhangsr <zhangsr@tiaohuo.com>
2018-06-09 18:57:43 +0800

    im

--- src/net/wit/controller/QrcodeController.java
+++ src/net/wit/controller/QrcodeController.java
@@ -1,10 +1,15 @@
 
 package net.wit.controller;
 
+import com.google.zxing.WriterException;
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGImageDecoder;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
 import net.wit.Message;
 import net.wit.entity.*;
 import net.wit.plugin.PaymentPlugin;
 import net.wit.service.*;
+import net.wit.util.QRBarCodeUtil;
 import net.wit.util.ScanUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,9 +18,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.util.Map;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
@@ -43,6 +50,36 @@ public class QrcodeController extends BaseController {
     @Resource(name = "adminServiceImpl")
     private AdminService adminService;
 
+    /**
+     * 根据
+     */
+    @RequestMapping(value = "/show", method = RequestMethod.GET)
+    public void show(String url, HttpServletRequest request, HttpServletResponse response) {
+        try {
+            String tempFile = System.getProperty("java.io.tmpdir") + "/upload_" + UUID.randomUUID() + ".jpg";
+            response.reset();
+            response.setContentType("image/jpeg;charset=utf-8");
+            try {
+                QRBarCodeUtil.encodeQRCode(url, tempFile, 200, 200);
+            } catch (WriterException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            ServletOutputStream output = response.getOutputStream();// 得到输出流
+            InputStream imageIn = new FileInputStream(new File(tempFile));
+            // 得到输入的编码器，将文件流进行jpg格式编码
+            JPEGImageDecoder decoder = JPEGCodec.createJPEGDecoder(imageIn);
+            // 得到编码后的图片对象
+            BufferedImage image = decoder.decodeAsBufferedImage();
+            // 得到输出的编码器
+            JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(output);
+            encoder.encode(image);// 对图片进行输出编码
+            imageIn.close();// 关闭文件流
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
      /**
       * 生成二维码
       */
