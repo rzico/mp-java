5d32d681ffffdea0da2949f87fad44fbb83fe778
rzico <zhangsr@051204>
2018-07-17 10:31:54 +0800

    water

--- src/net/wit/controller/makey/model/CourseModel.java
+++ src/net/wit/controller/makey/model/CourseModel.java
@@ -35,8 +35,33 @@ public class CourseModel extends BaseModel implements Serializable {
     /** 阅读数 */
     private Long hits;
 
-    /** 介绍 */
-    private String content;
+    /** 课程目标 */
+    private String content1;
+
+    /** 课程内容 */
+    private String content2;
+
+
+    /** 授课形式 */
+     private String content3;
+
+    /** 讲师头像 */
+    private String contentLogo;
+
+    /** 讲师简介 */
+    private String content4;
+
+    /** 课程大纲 */
+    private String content5;
+
+    /** 适合谁听 */
+    private String content6;
+
+    /** 您将获得 */
+    private String content7;
+
+    /** 往期回顾 图片json */
+    private String images;
 
     public Long getId() {
         return id;
@@ -86,12 +111,76 @@ public class CourseModel extends BaseModel implements Serializable {
         this.hits = hits;
     }
 
-    public String getContent() {
-        return content;
+    public String getContent1() {
+        return content1;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setContent1(String content1) {
+        this.content1 = content1;
+    }
+
+    public String getContent2() {
+        return content2;
+    }
+
+    public void setContent2(String content2) {
+        this.content2 = content2;
+    }
+
+    public String getContent3() {
+        return content3;
+    }
+
+    public void setContent3(String content3) {
+        this.content3 = content3;
+    }
+
+    public String getContentLogo() {
+        return contentLogo;
+    }
+
+    public void setContentLogo(String contentLogo) {
+        this.contentLogo = contentLogo;
+    }
+
+    public String getContent4() {
+        return content4;
+    }
+
+    public void setContent4(String content4) {
+        this.content4 = content4;
+    }
+
+    public String getContent5() {
+        return content5;
+    }
+
+    public void setContent5(String content5) {
+        this.content5 = content5;
+    }
+
+    public String getContent6() {
+        return content6;
+    }
+
+    public void setContent6(String content6) {
+        this.content6 = content6;
+    }
+
+    public String getContent7() {
+        return content7;
+    }
+
+    public void setContent7(String content7) {
+        this.content7 = content7;
+    }
+
+    public String getImages() {
+        return images;
+    }
+
+    public void setImages(String images) {
+        this.images = images;
     }
 
     public void bind(Course course) {
@@ -101,6 +190,14 @@ public class CourseModel extends BaseModel implements Serializable {
         this.hits = course.getHits();
         this.price = course.getPrice();
         this.signup = course.getSignup();
+        this.contentLogo = course.getContentLogo();
+        this.content1 = course.getContent1();
+        this.content2 = course.getContent2();
+        this.content3 = course.getContent3();
+        this.content4 = course.getContent4();
+        this.content5 = course.getContent5();
+        this.content6 = course.getContent6();
+        this.content7 = course.getContent7();
     }
 
     public static List<CourseModel> bindList(List<Course> courses) {
